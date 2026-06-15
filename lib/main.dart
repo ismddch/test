521c74a53229aac8d78fd7522a26de6a45290610
Mohamed <mohameddedech467@gmail.com>
2026-06-15 02:08:39 +0200

    Mon premier commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Image.network(""),
+        appBar: AppBar(
+          title: Text("med"),
+          backgroundColor: Color.fromARGB(121, 13, 45, 188)
+          ,),) ),);
+}
+
